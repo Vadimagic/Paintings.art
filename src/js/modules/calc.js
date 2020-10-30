a89df72b2027dee240b42eb455d7c168a8672bb8
Vadim <vadim.prog.2000@gmail.com>
2020-10-30 23:42:48 +0300

    finish calc

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -7,7 +7,22 @@ const calc = (size, material, options, promocode, result) => {
 
 	let sum = 0;
 
+	const calcFunction = () => {
+		sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+
+		if (sizeBlock.value == '' || materialBlock.value == '') {
+			resultBlock.textContent = 'Пожалуйста, выберите размер и материал картины';
+		} else if (promocodeBlock.value === 'IWANTPOPART') {
+			resultBlock.textContent = Math.round(sum * 0.7);
+		} else {
+			resultBlock.textContent = sum;
+		}
+	};
 	
+	sizeBlock.addEventListener('change', calcFunction);
+	materialBlock.addEventListener('change', calcFunction);
+	optionsBlock.addEventListener('change', calcFunction);
+	promocoeBlock.addEventListener('input', calcFunction);
 }
 
 export default calc
