41d17d7f85c43cede319d121ea13d8013dea596d
Vadim <vadim.prog.2000@gmail.com>
2020-11-05 21:15:43 +0300

    add events to inputs

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -1,5 +1,14 @@
 const drop = () => {
+	//drag *
+	//drahed *
+	//dragenter - объект над dropArea
+	//drageexit *
+	//dragleave - Объект перетащили за предлами dropArea
+	//dragover - Объект зависает над dropArea
+	//dragstart *
+	//drop - Объект отпущен в dropArea
 
+	const fileInputs = document.querySelectorAll('[name="upload"]');
 };
 
 export default drop;
